adc1649c8e8bc586021184f01e83da140907ca71
Farhan <arriqaaq@yahoo.com>
2018-09-10 20:02:25 +0530

    test case for intial chord ring check

---
+++ examples/n3.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"github.com/arriqaaq/chord"
+	"github.com/arriqaaq/chord/internal"
+	"log"
+	"math/big"
+	"os"
+	"os/signal"
+	// "strconv"
+	"time"
+)
+
+func createNode(id string, addr string, sister *internal.Node) (*chord.Node, error) {
+
+	cnf := chord.DefaultConfig()
+	cnf.Id = id
+	cnf.Addr = addr
+	cnf.Timeout = 10 * time.Millisecond
+	cnf.MaxIdle = 100 * time.Millisecond
+
+	n, err := chord.NewNode(cnf, sister)
+	// log.Println("hook", id, n, err)
+	return n, err
+}
+
+func createID(id string) []byte {
+	val := big.NewInt(0)
+	val.SetString(id, 10)
+	return val.Bytes()
+}
+
+func main() {
+
+	id1 := "1"
+	sister := chord.NewInode(id1, "0.0.0.0:8001")
+
+	h, err := createNode("8", "0.0.0.0:8003", sister)
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+
+	// shut := make(chan bool)
+	// var count int
+	// go func() {
+	// 	ticker := time.NewTicker(3 * time.Second)
+	// 	for {
+	// 		select {
+	// 		case <-ticker.C:
+	// 			count++
+	// 			key := strconv.Itoa(count)
+	// 			val, gErr := h.Get(key)
+	// 			if gErr != nil {
+	// 				log.Println("err: ", key, gErr)
+	// 			} else {
+	// 				log.Println("val: ", string(val))
+	// 			}
+	// 		case <-shut:
+	// 			ticker.Stop()
+	// 			return
+	// 		}
+	// 	}
+	// }()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+	// shut <- true
+	h.Stop()
+}
